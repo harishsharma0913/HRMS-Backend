be854f23c0f496b2c318fea2b7fdf64787b3ce32
Harish Sharma <harishsharma0913@gmail.com>
2025-12-04 17:36:42 +0530

    task get api change

--- src/Controllers/taskController.js
+++ src/Controllers/taskController.js
@@ -41,8 +41,8 @@ exports.createTask = async (req, res) => {
 // ---------------------- GET ALL TASKS ----------------------
 exports.getAllTasks = async (req, res) => {
   try {
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
+    const page = Number(req.query.page);
+    const limit = Number(req.query.limit);
     const skip = (page - 1) * limit;
 
     let filter = {};
